fb41d0889245013a0fb879bbd5c6c7650d76dced
Braian Emmanuel Carabajal <braiancarabajal@gmail.com>
2023-08-09 10:31:51 -0300

    Added "See more" button in OurServices

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -56,7 +56,7 @@ const OurServices = () => {
 
   return (
     <div className="flex justify-center items-center pb-[72px]">
-      <div className="flex flex-col items-center  bg-[#3E5D9D] px-[1.25rem] md:px-[59px] py-[4.5rem] rounded-[2.5rem] w-[90vw] gap-[2.5rem] shadow-2xl">
+      <div className="flex flex-col items-center bg-[#3E5D9D] px-[1.25rem] md:px-[59px] py-[4.5rem] rounded-[2.5rem] w-[90vw] gap-[2.5rem] shadow-2xl">
         <div className="flex flex-col items-center gap-[1.8125rem] self-stretch">
           <h2 className="text-center text-white text-[2.5rem] font-bold tracking-tight">
             Our Services
@@ -116,11 +116,7 @@ const OurServices = () => {
                 key={item.id}
                 className={`${
                   i === 0 || i === 1 ? "lg:hidden" : ""
-                } flex flex-col items-center py-[2.5rem] px-[1.875rem] gap-[1.25rem] self-stretch rounded-[20px] bg-[#F9F9F9] overflow-hidden`}
-                onClick={() => {
-                  setOpen(true);
-                  setModalItem(item);
-                }}
+                } flex flex-col items-center lg:justify-between py-[2.5rem] px-[1.875rem] gap-[1.25rem] self-stretch rounded-[20px] bg-[#F9F9F9] overflow-hidden`}
               >
                 <span className="flex flex-col justify-center items-center gap-[10px] w-[72px] h-[72px] rounded-[2000px]">
                   <Image
@@ -137,9 +133,19 @@ const OurServices = () => {
                     {item.title}
                   </h4>
 
-                  <p className="self-stretch text-center text-[16px] text-[#242424] servicesEllipsis">
+                  <p className="self-stretch text-center text-[16px] h-[96px] text-[#242424] servicesEllipsis">
                     {lines[0].trim()}
                   </p>
+
+                  <button
+                    onClick={() => {
+                      setOpen(true);
+                      setModalItem(item);
+                    }}
+                    className="bg-[#6C9DD7] text-white shadow-lg rounded-[35px] py-[13px] w-full text-[16px] font-bold tracking-[0.16px] transition-all active:scale-90"
+                  >
+                    See more
+                  </button>
                 </span>
               </div>
             );
